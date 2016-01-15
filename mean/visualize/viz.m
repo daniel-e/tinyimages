9055fa74e550505782dc60c2a3708abbfe293457
de <git.daniele@gmail.com>
2016-01-15 13:04:50 +0100

    mean image

--- mean/visualize/viz.m
+++ mean/visualize/viz.m
@@ -2,10 +2,12 @@
 
 x = load('../outputs/mean.txt');
 imshow(reshape(x / max(x), [32, 32, 3]));
+print -dpng 'outputs/mean.png'
 disp('press enter to continue');
 pause;
 y = x - min(x);
 y = y / max(y);
 imshow(reshape(y, [32, 32, 3]));
+print -dpng 'outputs/mean_stretched.png'
 disp('press enter to quit');
 pause;
